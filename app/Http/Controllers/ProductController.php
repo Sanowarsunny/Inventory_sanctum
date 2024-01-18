c7144c4cfeb3171cbad6d1eb6799957615fb5e48
Sanowarsunny <sanowarsunny1998@gmail.com>
2024-01-18 23:54:21 +0600

    error fixing

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,51 +11,68 @@ use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
-    public function productPage():View{
+    public function productPage(): View
+    {
         return view('pages.dashboard.product-page');
     }
-    public function listProducts(){
-        try{
-             $user_id = Auth::id();
-            $rows = Product::where('user_id',$user_id)->get();
+    public function listProducts()
+    {
+        try {
+            $user_id = Auth::id();
+            $rows = Product::where('user_id', $user_id)->get();
             return response()->json(['status' => 'success', 'rows' => $rows]);
-        }
-        catch(Exception $e){
+        } catch (Exception $e) {
             return response()->json(
                 [
-                    'status'=>"Fail",
-                    'message'=>$e->getMessage(),
+                    'status' => "Fail",
+                    'message' => $e->getMessage(),
 
                 ]
             );
         }
     }
 
-    public function createProduct(Request $request){
-        //$user_id=$request->header('id');
-        $user_id = Auth::id();
-        //dd($user_id);
-        // Prepare File Name & Path
-        $img=$request->file('img');
+    public function createProduct(Request $request)
+    {
 
-        $t=time();
-        $file_name=$img->getClientOriginalName();
-        $img_name="{$user_id}-{$t}-{$file_name}";
-        $img_url="uploads/{$img_name}";
+        try { //$user_id=$request->header('id');
+            $user_id = Auth::id();
+            //dd($user_id);
+            // Prepare File Name & Path
+            $img = $request->file('img');
 
-
-        // Upload File
-        $img->move(public_path('uploads'),$img_name);
+            $t = time();
+            $file_name = $img->getClientOriginalName();
+            $img_name = "{$user_id}-{$t}-{$file_name}";
+            $img_url = "uploads/{$img_name}";
 
 
-        // Save To Database
-        return Product::create([
-            'name'=>$request->input('name'),
-            'price'=>$request->input('price'),
-            'unit'=>$request->input('unit'),
-            'img_url'=>$img_url,
-            'category_id'=>$request->input('category_id'),
-            'user_id'=>$user_id
-        ]);
+            // Upload File
+            $img->move(public_path('uploads'), $img_name);
+
+
+            // Save To Database
+
+            $product = Product::create([
+                'name' => $request->input('name'),
+                'price' => $request->input('price'),
+                'unit' => $request->input('unit'),
+                'img_url' => $img_url,
+                'category_id' => $request->input('category_id'),
+                'user_id' => $user_id
+            ]);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Request success',
+                'product' => $product
+            ]);
+        } 
+        catch (Exception $e) {
+            return response()->json([
+                'status' => "fail",
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 }
